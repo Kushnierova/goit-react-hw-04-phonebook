6548af8603c77ace28ccb5c2e18097241195f00b
Kushnierova <Katerynakushnierova@gmail.com>
2024-01-04 21:45:41 +0100

    21:45

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -63,70 +63,7 @@ function ContactForm({onSubmit}) {
     </form>
   );
 }
-// class OldContactForm extends Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
 
-//   onSubmit = this.props.onSubmit;
-
-//   nameInputId = nanoid();
-//   numberInputId = nanoid();
-
-//   handleChange = e => {
-//     const { name, value } = e.currentTarget;
-//     this.setState({
-//       [name]: value,
-//     });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-
-//     this.props.onSubmit(this.state.name, this.state.number);
-//     this.reset();
-//   };
-
-//   reset = () => {
-//     this.setState({ name: '', number: '' });
-//   };
-
-//   render() {
-//     const { name, number } = this.state;
-//     return (
-//       <form className={css.form} onSubmit={this.handleSubmit}>
-//         <label className={css.label} htmlFor={this.nameInputId}>
-//           Name:{' '}
-//           <input
-//             type="text"
-//             name="name"
-//             value={name}
-//             required
-//             onChange={this.handleChange}
-//             className={css.input}
-//             id={this.nameInputId}
-//           />
-//         </label>
-//         <label className={css.label} htmlFor={this.numberInputId}>
-//           Number:
-//           <input
-//             type="tel"
-//             name="number"
-//             value={number}
-//             required
-//             onChange={this.handleChange}
-//             className={css.input}
-//             id={this.numberInputId}
-//           />
-//         </label>
-//         <button className={css.btn} type="submit">
-//           Add contact
-//         </button>
-//       </form>
-//     );
-//   }
-// }
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
